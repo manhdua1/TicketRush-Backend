955b2072cd20ddb6285b9d32fa4c1770ee6a0e96
manhdua1 <manhvu20012005@gmail.com>
2026-04-09 18:47:11 +0700

    Initialize Spring project

---
+++ src/test/java/com/ticketrush/backend/TicketrushBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ticketrush.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketrushBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
