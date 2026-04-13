30af852554a10d241b63a70b044afdb2b4dc2700
manhdua1 <manhvu20012005@gmail.com>
2026-04-14 00:40:00 +0700

    Add ErrorCode enum

---
+++ src/main/java/com/ticketrush/backend/exception/ErrorCode.java
@@ -0,0 +1,24 @@
+package com.ticketrush.backend.exception;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.experimental.FieldDefaults;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+
+@Getter
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+public enum ErrorCode {
+    UNCATEGORIZED_EXCEPTION(9999, "Lỗi không xác định", HttpStatus.INTERNAL_SERVER_ERROR)
+    ;
+
+    ErrorCode(int code, String message, HttpStatusCode statusCode) {
+        this.code = code;
+        this.message = message;
+        this.statusCode = statusCode;
+    }
+
+    int code;
+    String message;
+    HttpStatusCode statusCode;
+}
