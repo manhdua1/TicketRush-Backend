de0e9c2e934659d964f198b1fe558844f46f341e
manhdua1 <manhvu20012005@gmail.com>
2026-04-27 10:16:15 +0700

    Add UserService and get user by id method

---
+++ src/main/java/com/ticketrush/backend/service/UserService.java
@@ -0,0 +1,27 @@
+package com.ticketrush.backend.service;
+
+import com.ticketrush.backend.dto.response.UserResponse;
+import com.ticketrush.backend.entity.User;
+import com.ticketrush.backend.exception.AppException;
+import com.ticketrush.backend.exception.ErrorCode;
+import com.ticketrush.backend.mapper.UserMapper;
+import com.ticketrush.backend.repository.UserRepository;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+public class UserService {
+    UserRepository userRepository;
+    UserMapper userMapper;
+
+    public UserResponse getUserById(Integer id) {
+        User user = userRepository.findById(id)
+                .orElseThrow(() -> new AppException(ErrorCode.USER_NOT_FOUND));
+
+        return userMapper.toUserResponse(user);
+    }
+}
