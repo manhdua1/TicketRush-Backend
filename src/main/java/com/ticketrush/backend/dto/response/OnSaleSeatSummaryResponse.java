1f1becbf954f76e535b3d3fb5308e98b3171eec5
manhdua1 <manhvu20012005@gmail.com>
2026-05-17 00:33:18 +0700

    Add on sale seat summary and on sale events count api

---
+++ src/main/java/com/ticketrush/backend/dto/response/OnSaleSeatSummaryResponse.java
@@ -0,0 +1,17 @@
+package com.ticketrush.backend.dto.response;
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.experimental.FieldDefaults;
+
+@Getter
+@Setter
+@Builder
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class OnSaleSeatSummaryResponse {
+    Long soldSeats;
+    Long totalSeats;
+    Double soldRate;
+}
